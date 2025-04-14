9044226a72c980cf8401b0474283ef1853130c78
mvigneshPrasath <annamammnbj>
2025-04-14 22:04:42 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,61 @@
+
+document.getElementById("butt1").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/validations.git';
+   };
+  document.getElementById("butt1v").onclick = function () {
+    location.href ='https://annamalai9585.github.io/validations/';
+  };
+  
+document.getElementById("butt2").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/project-Ayna.git';
+   };
+  document.getElementById("butt2v").onclick = function () {
+    location.href = 'https://annamalai9585.github.io/project-Ayna/';
+  };
+
+  document.getElementById("butt3").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/Loading.git';
+   };
+  document.getElementById("butt3v").onclick = function () {
+    location.href = 'https://annamalai9585.github.io/Loading/';
+  };
+
+  document.getElementById("butt4").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/Testimonal.git';
+   };
+  document.getElementById("butt4v").onclick = function () {
+    location.href = 'https://annamalai9585.github.io/Testimonal/';
+  };
+
+  document.getElementById("butt5").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/clock.git';
+   };
+  document.getElementById("butt5v").onclick = function () {
+    location.href = 'https://annamalai9585.github.io/clock/';
+  };
+
+  document.getElementById("butt6").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/calculator.git';
+   };
+  document.getElementById("butt6v").onclick = function () {
+    location.href = 'https://annamalai9585.github.io/calculator/';
+  };
+
+  document.getElementById("butt7").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/Todo.git';
+   };
+  document.getElementById("butt7v").onclick = function () {
+    location.href = 'https://annamalai9585.github.io/Todo/';
+  };
+  
+  document.getElementById("butt8").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/Meesho.git';
+   };
+  document.getElementById("butt8v").onclick = function () {
+    location.href = 'https://annamalai9585.github.io/Meesho/';
+  };
+
+
+
+
+  
